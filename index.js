36b3c80234aa5deda10c767eb572d9ac40f3af62
j6646391-web <j6646391@gmail.com>
2025-10-28 09:24:33 +0700

    create index.js

---
+++ index.js
@@ -0,0 +1,242 @@
+/** !-======[ Experimentall ▪︎ Bell🦋 ]======-!
+      * Coding by @rifza.p.p *  
+      * Makasih BTW SCnya saya Develop lagi ya *
+      
+      🩵 Follow ️me on :
+      ▪︎ https://youtube.com/@rifza  
+      ▪︎ https://github.com/Rifza123
+      ▪︎ https://instagram.com/rifza.p.p?igshid=ZGUzMzM3NWJiOQ==
+      ▪︎ https://www.threads.net/@rifza.p.p
+      ▪︎ https://termai.cc
+      ▪︎ https://xterm.tech
+  */
+/*!-======[ Preparing Configuration ]======-!*/
+import './toolkit/set/prototype.js';
+let { initialize } = await './toolkit/set/global.js'.r();
+
+/*!-======[ Mudules Imports ]======-!*/
+const readline = 'readline'.import();
+const fs = 'fs'.import();
+const chalk = 'chalk'.import();
+const baileys = 'baileys'.import();
+const pino = 'pino'.import();
+const { Boom } = 'boom'.import();
+const { Connecting } = await `${fol[8]}systemConnext.js`.r();
+const Event = (await 'events'.import()).default;
+let { makeInMemoryStore } = await `${fol[0]}store.js`.r();
+
+Event.defaultMaxListeners = 25;
+
+let {
+  makeWASocket,
+  useMultiFileAuthState,
+  DisconnectReason,
+  getContentType,
+  Browsers,
+} = baileys;
+
+await initialize(); //db
+/*!-======[ Functions Imports ]======-!*/
+let detector = (await (fol[0] + 'detector.js').r()).default;
+Data.utils = (await `${fol[1]}utils.js`.r()).default;
+Data.helper = (await `${fol[1]}client.js`.r()).default;
+Data.In = (await `${fol[1]}interactive.js`.r()).default;
+Data.reaction = (await `${fol[1]}reaction.js`.r()).default;
+Data.EventEmitter = (await `${fol[1]}events.js`.r()).default;
+Data.stubTypeMsg = (await `${fol[1]}stubTypeMsg.js`.r()).default;
+Data.eventGame = (await `${fol[1]}eventGame.js`.r()).default;
+
+Data.initialize = (await `${fol[1]}initialize.js`.r()).default;
+
+let logger = pino({ level: 'silent' });
+let store = makeInMemoryStore();
+
+async function launch() {
+  try {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    const question = (text) =>
+      new Promise((resolve) => rl.question(text, resolve));
+    if (fs.existsSync(session) && !fs.existsSync(session + '/creds.json'))
+      await fs.rmdir(session, { recursive: true }, (err) => {});
+    if (!fs.existsSync(session + '/creds.json')) {
+      let quest = `\n${chalk.red.bold('╭──────────────────────────────────────────────────────╮')}\n${chalk.red.bold('│')} ${chalk.bold('❗️ Anda belum memiliki session ❗️')} ${chalk.red.bold('│')}\n${chalk.red.bold('╰──────────────────────────────────────────────────────╯')}\n            \n${chalk.green('🏷 Pilih salah satu dari opsi berikut untuk menautkan perangkat:')}\n${chalk.blue('▪︎ qr')}\n${chalk.blue('▪︎ pairing')}\n\n${chalk.yellow('* Ketik salah satu dari opsi di atas, contoh:')} ${chalk.blue.bold('pairing')}\n\n${chalk.yellow('Please type here: ')}`;
+
+      await sleep(1000);
+      const opsi = await question(quest);
+      if (opsi == '2') {
+        global.pairingCode = true;
+      } else if (opsi == '1') {
+        global.pairingCode = false;
+      } else {
+        console.log(`Pilihan opsi tidak tersedia!`);
+      }
+    }
+
+    let { state, saveCreds } = await useMultiFileAuthState(session);
+    const Exp = makeWASocket({
+      logger,
+      version: [2,3000,1025150051],
+      printQRInTerminal: !global.pairingCode,
+      browser: Browsers.ubuntu('Brave'),
+      auth: state
+    });
+
+    if (global.pairingCode && !Exp.authState.creds.registered) {
+      const phoneNumber = await question(
+        chalk.yellow('Please type your WhatsApp number : ')
+      );
+      let code = await Exp.requestPairingCode(
+        phoneNumber.replace(/[+ -]/g, '')
+      );
+      console.log(
+        chalk.bold.rgb(
+          255,
+          136,
+          0
+        )(
+          `\n  ╭────────────────────────────╮\n  │  ${chalk.yellow('Your Pairing Code:')} ${chalk.greenBright(code)}  │\n  ╰────────────────────────────╯\n            `
+        )
+      );
+    }
+
+    /*!-======[ INITIALIZE ]======-!*/
+    Data.initialize({ Exp, store }); //Exp function
+
+    /*!-======[ Detect File Update ]======-!*/
+    keys['detector'] && clearInterval(keys['detector']);
+    detector({ Exp, store });
+
+    /*!-======[ EVENTS Exp ]======-!*/
+    Exp.ev.on('connection.update', async (update) => {
+      await Connecting({ update, Exp, Boom, DisconnectReason, sleep, launch });
+    });
+
+    Exp.ev.on('creds.update', saveCreds);
+    Exp.ev.on('message-receipt.update', async (msg) => {
+      /* console.log(msg)
+     la  | [
+0|bella  |   {
+0|bella  |     key: {
+0|bella  |       remoteJid: '120363145935286949@g.us',
+0|bella  |       id: '3EB0B93C02C7C41AAE9549',
+0|bella  |       fromMe: true,
+0|bella  |       participant: '6283110928302@s.whatsapp.net'
+0|bella  |     },
+0|bella  |     receipt: {
+0|bella  |       userJid: '6283110928302@s.whatsapp.net',
+0|bella  |       readTimestamp: 1755108157
+0|bella  |     }
+0|bella  |   },
+0|bella  |   {
+0|bella  |     key: {
+0|bella  |       remoteJid: '120363145935286949@g.us',
+0|bella  |       id: '3EB0674A48121D16F94671',
+0|bella  |       fromMe: true,
+0|bella  |       participant: '6283110928302@s.whatsapp.net'
+0|bella  |     },
+0|bella  |     receipt: {
+0|bella  |       userJid: '6283110928302@s.whatsapp.net',
+0|bella  |       readTimestamp: 1755108157
+0|bella  |     }
+0|bella  |   }
+0|bella  | ]*/
+    });
+    Exp.ev.on('messages.upsert', async ({ type, messages }) => {
+      
+        for (let message of messages) {
+          const cht = {
+            ...message,
+            id: message.key.remoteJid,
+          };
+          let { messageTimestamp } = cht
+          if(typeof messageTimestamp == 'object' && messageTimestamp.unsigned) continue 
+          
+          let isMessage = cht?.message;
+          let isStubType = cht?.messageStubType;
+          if (!(isMessage || isStubType)) return;
+          if (cht.key.remoteJid === 'status@broadcast') {
+            if (!cfg.reactsw)
+              cfg.reactsw = {
+                on: false,
+                emojis: ['😍', '😂', '😬', '🤢', '🤮', '🥰', '😭'],
+              };
+
+            if (cfg.reactsw.on) {
+              let { emojis } = cfg.reactsw;
+              await Exp.sendMessage(
+                cht.id,
+                { react: { key: cht.key, text: emojis.getRandom() } },
+                {
+                  statusJidList: [
+                    cht.key.participant,
+                    Exp.user.id.split(':')[0] + from.sender,
+                  ],
+                }
+              );
+            } else if (cfg.autoreadsw == true) {
+              await Exp.readMessages([cht.key]);
+              let typ = getContentType(cht.message);
+              console.log(
+                /protocolMessage/i.test(typ)
+                  ? `${cht.key.participant.split('@')[0]} Deleted story❗`
+                  : 'View user stories : ' + cht.key.participant.split('@')[0]
+              );
+            }
+            return;
+          } else {
+            const exs = { cht, Exp, is: {}, store };
+            let action = await Data.utils(exs);
+            switch (action) {
+              case 'NEXT':
+                if (type == 'append') {
+                  Data.stubTypeMsg(exs);
+                }
+                else if (type == 'notify') {
+                  await Data.helper(exs);
+                }
+                break;
+
+              case 'ERROR':
+                console.error(
+                  '\x1b[31mERROR in utils.js: cek error di atas, segera laporkan ke admin/owner\x1b[0m'
+                );
+                break;
+            }
+          
+        }
+      }
+    });
+
+    Exp.ev.on('call', async ([c]) => {
+      let { from, id, status } = c;
+      if (status !== 'offer') return;
+      cfg.call = cfg.call || { block: false, reject: false };
+      let { block, reject } = cfg.call;
+      if (reject) {
+        await Exp.rejectCall(id, from);
+        await Exp.sendMessage(from, { text: '⚠️JANGAN TELPON❗' });
+      }
+      if (block) {
+        let text =
+          `\`⚠️KAMU TELAH DI BLOKIR!⚠️\`` +
+          '\n- *Menelpon tidak diizinkan karena sangat mengganggu aktivitas kami*' +
+          '\n> _Untuk membuka blokir, silahkan hubungi owner!_';
+        await Exp.sendMessage(from, { text });
+        await Exp.sendContacts({ id: from }, owner);
+        await sleep(2000);
+        await Exp.updateBlockStatus(from, 'block');
+      }
+    });
+    store.bind(Exp.ev);
+  } catch (error) {
+    console.error(error);
+  }
+}
+launch();
+process.on('uncaughtException', (e) => {
+  console.error(e);
+});
